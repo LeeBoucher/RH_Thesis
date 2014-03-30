67511990ba8a1eba9c13e0eeb29971172218da31
LeeBoucher <Lee.Evan.Boucher@gmail.com>
2014-03-30 02:55:57 +0000

    Python script to put data in S3 storage and kill instance when simulations finish

---
+++ Local/AWS_script.py
@@ -0,0 +1,26 @@
+
+import json
+
+
+# TODO get the values from the local config file
+
+
+# TODO The R script should run in here
+
+
+# import boto
+# from boto.s3.connection import S3Connection
+# conn = S3Connection('<aws access key>', '<aws secret key>')
+
+
+# b = conn.get_bucket('boucherthesissimulation')
+# from boto.s3.key import Key
+# k = Key(bucket)
+# k.key = 'simulation_results'
+# #TODO the rest of this S3 stuff
+
+
+# import os
+# os.system("shutdown now -h")
+
+
